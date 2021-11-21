e9a345b65e146331b5f76ecc6949786743b7c546
Ethan Creagar <ethan@Ethans-iMac.local>
2021-11-21 15:37:54 -0700

    added script for subsetting the punt returns

---
+++ subset_plays.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from random import sample
+data = pd.read_csv('puntreturns>>> data = pd.read_csv('puntreturns.csv')
+plays = sample(list(set(data['playId'])), len(set(data['playId']))//100) # subset 1/100th of the data
+subset = data.loc[data['playId'].isin(plays)]
+subset.to_csv('punts_1percent.csv')
